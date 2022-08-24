5b5ab50774f7f5573d5128b0bccb3fbce3aeb18a
agorski2 <adamsebastiangorski@gmail.com>
2022-08-24 18:36:40 +0200

    setting up repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Recommendation engine for spotify million playlist challange',
+    author='Adam Górski',
+    license='BSD-3',
+)
